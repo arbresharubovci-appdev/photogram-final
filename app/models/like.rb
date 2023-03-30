3ca527dab33b3a1f81b0fbb908f66efdf89f0e97
Arbreshë Rubovci <arbresharubovci@gmail.com>
2023-03-30 21:35:27 +0000

    Rails generate is add

---
+++ app/models/like.rb
@@ -0,0 +1,12 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  fam_id     :integer
+#  photo_id   :integer
+#
+class Like < ApplicationRecord
+end
