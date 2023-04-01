20cbd2c033affd919313f646c5397ddbb6ba6c13
Arbreshë Rubovci <arbresharubovci@gmail.com>
2023-04-01 16:13:38 +0000

    add image uploder

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -18,12 +18,13 @@ class PhotosController < ApplicationController
   end
 
   def create
+    
     the_photo = Photo.new
     the_photo.caption = params.fetch("query_caption")
-    the_photo.comments_count = params.fetch("query_comments_count")
-    the_photo.image = params.fetch("query_image")
-    the_photo.owner_id = params.fetch("query_owner_id")
-    the_photo.likes_count = params.fetch("query_likes_count")
+    the_photo.comments_count = 0
+    the_photo.image = params.fetch(:image)
+    the_photo.likes_count = 0
+    the_photo.owner_id = @current_user.id
 
     if the_photo.valid?
       the_photo.save
@@ -31,6 +32,7 @@ class PhotosController < ApplicationController
     else
       redirect_to("/photos", { :alert => the_photo.errors.full_messages.to_sentence })
     end
+
   end
 
   def update
