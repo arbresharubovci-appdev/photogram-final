918759b9ebe2b46cce3d55a9abe11b201f93b143
Arbreshë Rubovci <arbresharubovci@gmail.com>
2023-04-02 17:12:05 +0000

    the project is done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,18 +16,29 @@ class ApplicationController < ActionController::Base
     end
   end
 
-
   def index
     matching_users = User.all
+
     @list_of_users = matching_users.order({ :username => :asc })
+
+
+
+
     render({ :template => "users/index.html.erb" })
   end
 
   def show
+
+
     the_username = params.fetch("path_username")
+
     matching_users = User.where({ :username => the_username })
+
     @the_user = matching_users.at(0)
+
+
     matching_follow_requests = FollowRequest.all
+
     @list_of_follow_requests = matching_follow_requests.order({ :created_at => :desc })
 
     if @current_user == nil
@@ -35,36 +46,25 @@ class ApplicationController < ActionController::Base
     else
     render({ :template => "users/show.html.erb" })
     end
-
-    def liked_photos
-      @photos = @current_user.photos
-      render({ :template => "users/liked_photos.html.erb" })
-    end
-  
-    def feed
-      render({ :template => "users/feed.html.erb" })
-    end
-  
-    def discover
-      render({ :template => "users/discover.html.erb" })
-    end
   end
 
- 
-  
-  # Uncomment line 5 in this file and line 3 in UserAuthenticationController if you want to force users to sign in before any other actions.
-  #before_action(:force_user_sign_in)
-  
-  def load_current_user
-    the_id = session[:user_id]
-    
-    @current_user = User.where({ :id => the_id }).first
+  def liked_photos
+    @photos = @current_user.photos
+
+    render({ :template => "users/liked_photos.html.erb" })
   end
-  
-  def force_user_sign_in
-    if @current_user == nil
-      redirect_to("/user_sign_in", { :notice => "You have to sign in first." })
-    end
+
+  def feed
+
+
+    render({ :template => "users/feed.html.erb" })
   end
+
+  def discover
+
+
+    render({ :template => "users/discover.html.erb" })
+  end
+
 
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -22,7 +22,6 @@ class PhotosController < ApplicationController
   end
 
   def create
-    
 
     the_photo = Photo.new
     the_photo.caption = params.fetch("query_caption")
@@ -43,12 +42,11 @@ class PhotosController < ApplicationController
   def update
     the_id = params.fetch("path_id")
     the_photo = Photo.where({ :id => the_id }).at(0)
-
     the_photo.caption = params.fetch("query_caption")
     the_photo.comments_count = params.fetch("query_comments_count")
     the_photo.image = params.fetch("query_image")
-    the_photo.likes_count = params.fetch("query_likes_count")
     the_photo.owner_id = params.fetch("query_owner_id")
+    the_photo.likes_count = params.fetch("query_likes_count")
 
     if the_photo.valid?
       the_photo.save
